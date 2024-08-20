82b572a526dbf68158aa2a2d21030d023c1a570c
Kitae's Github <aqua0405@gmail.com>
2024-08-20 23:40:26 +0900

    240820-2340

--- 03_DOM/ex1/app.js
+++ 03_DOM/ex1/app.js
@@ -3,24 +3,15 @@
 //=====================================
 'use strict';
 
-const h1 = document.querySelector('h1');
-console.log(h1.textContent);
-h1.textContent = 'Hello, Learn DOM!';
-h1.className = 'title';
+const liFirst = document.querySelector('li');
+console.log(liFirst.parentNode); // ul
+console.log(liFirst.parentElement); // ul
 
-h1.style.color = 'white';
-h1.style.backgroundColor = 'black';
+console.log(liFirst.closest('body')); // body
+console.log(liFirst.closest('header')); // ul
 
-const input = document.querySelector('input');
-input.value = 'Hello, DOM!';
-input.setAttribute('value', 'Hello, Kitae!'); // value 속성을 설정, 하지만 화면에는 반영되지 않음
-input.value = input.getAttribute('value');    // value 속성을 가져와서 input 요소의 value 속성에 대입
-console.log(input.value);
+const ul = liFirst.parentElement;
+console.log(ul); // ul 
 
-const li = document.querySelector('li:last-of-type');
-li.textContent = 'JavaScript';
-
-const listItemElements = document.querySelectorAll('li');
-for(const listElement of listItemElements) {
-  console.dir(listElement);
-}
+console.log(ul.previousElementSibling); // header 
+console.log(ul.nextElementSibling); // input 
