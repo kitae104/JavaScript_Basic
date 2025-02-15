d6b100ea7473771cc4dc7bb927aca2f2a958be0b
Kitae's Github <aqua0405@gmail.com>
2025-02-16 00:39:37 +0900

    Update objects.js

--- 05_Object/assets/scripts/objects.js
+++ 05_Object/assets/scripts/objects.js
@@ -3,12 +3,15 @@ const movieList = document.querySelector('#movie-list');
 movieList.style['background-color'] = 'red';
 movieList.style.display = 'block';
 
+const userChosenKeyName = 'level';
+
 const person = {
-  name: 'Kitae',
+  'first name': 'Kitae',
   age: 27,
   hobbies: ['Sports', 'Cooking'],
   num: 100,
   1.5: 'hello',
+  [userChosenKeyName]: '123',
   greet: function() {
     alert('Hi there!');
   }
@@ -27,4 +30,10 @@ console.log(person[1.5]); // 접근 방법 '1.5'도 가능 (하지만 권장하�
 const numbers = {5: 'five', 1: 'one'};
 console.log(numbers); // {1: "one", 5: "five"} 순서가 오름차순 
 
-
+const keyName = 'first name';
+console.log(person[keyName]); // Kitae
+console.log(person['first name']); // Kitae
+console.log(person.keyName); // undefined 
+console.log(person[userChosenKeyName]); // 123
+console.log(person['level']); // 123
+console.log(person.userChosenKeyName); // undefined
